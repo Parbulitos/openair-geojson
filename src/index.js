5f6834146e68ebf0ef5dd5085951a60ff440de04
Parbulitos <cechavarne@gmail.com>
2024-05-15 11:24:34 +0200

    fixing npm publish

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseToGeojson = void 0;
-const parser_1 = __importDefault(require("./parser"));
-exports.parseToGeojson = parser_1.default;
